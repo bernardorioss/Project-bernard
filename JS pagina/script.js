f226795ac5b0ff7d515bd102a6b46941a7a385d7
Bernardo Rios <bernardo_palmeira@hotmail.com>
2022-11-21 15:38:43 -0300

    quase tudo pronto

--- JS pagina/script.js
+++ JS pagina/script.js
@@ -1,4 +1,39 @@
-function sair() {
-    localStorage.removeItem('token')
-    window.location.href = "login.html";
+function ir() {
+    window.location.href = "hotel.html";
 }
+
+function ela() {
+    window.location.href = "navbar.html";
+}
+
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
